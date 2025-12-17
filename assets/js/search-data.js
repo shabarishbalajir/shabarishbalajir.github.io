0446cd6d0b19c57e0e0df494606981c22e494e7e
shabarishbalajir <shabarishbalaji@gmail.com>
2025-12-17 12:01:15 +0000

    Deploying to gh-pages from @ shabarishbalajir/shabarishbalajir.github.io@7ebdd920d0e4b8e9ec8f067b4d18f6e4eeb17a3e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -499,14 +499,14 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
+          window.open("/assets/pdf/Shabarish_Balaji_PhD_Caltech_CMS_Resume.pdf", "_blank");
         },
       },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%73%68%61%62%61%72%69%73%68%62%61%6C%61%6A%69@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -516,25 +516,11 @@ ninja.data = [{
           window.open("https://inspirehep.net/authors/1010907", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=TMFtBsgAAAAJ&hl", "_blank");
         },
       },{
       id: 'light-theme',
